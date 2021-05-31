22b9ee8cf7704687771061ac241cdd27e2343574
Sumit-the-coder <sumitava.biswas@tieto.com>
2021-05-31 17:37:23 +0530

    unused comments

--- vaccince/src/main/java/com/sum/vaccince/VaccinceApplication.java
+++ vaccince/src/main/java/com/sum/vaccince/VaccinceApplication.java
@@ -27,8 +27,6 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class VaccinceApplication {
 
-
-
 	@Autowired
 	ApplicationContext appContext;
 
@@ -37,69 +35,10 @@ public class VaccinceApplication {
 	}
 
 	@PostConstruct
-	public void postConstract(){
-		if(appContext.containsBean("apiCall")){
+	public void postConstract() {
+		if (appContext.containsBean("apiCall")) {
 			System.out.println("We got the bean");
 		}
 	}
 
-//	@Bean
-//	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-//		return builder.build();
-//	}
-//
-//	@Bean
-//	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-//		System.out.println("Scheduler is starting");
-//		return args -> {
-//			String quote = restTemplate.getForObject( BY_DISTRICT, String.class);
-//			Map<String, Sessions> map = new HashMap<>();
-//			map = new Gson().fromJson(quote, Map.class);
-//
-//			ArrayList<Sessions> listdata = new ArrayList<Sessions>();
-//			JSONObject jsonObject = new JSONObject(quote);
-//			JSONArray jsonArray = jsonObject.getJSONArray("sessions");
-//			if(jsonArray != null) {
-//				for (int i = 0; i < jsonArray.length(); i++) {
-//					JSONObject jobj = (JSONObject) jsonArray.get(i);
-//					Sessions s = new Sessions();
-//					s.setFee(jobj.getString("fee"));
-//					s.setPincode(jobj.getString("pincode"));
-//					s.setFee_type(jobj.getString("fee_type"));
-//					s.setAddress(jobj.getString("address"));
-//					s.setAvailable_capacity(jobj.getInt("available_capacity"));
-//					s.setAvailable_capacity_dose1(jobj.getString("available_capacity_dose1"));
-//					s.setAvailable_capacity_dose2(jobj.getString("available_capacity_dose2"));
-//					s.setBlock_name(jobj.getString("block_name"));
-//					s.setCenter_id(jobj.getString("center_id"));
-//					s.setDate(jobj.getString("date"));
-//					s.setDistrict_name(jobj.getString("district_name"));
-//					s.setFrom(jobj.getString("from"));
-//					s.setMin_age_limit(jobj.getString("min_age_limit"));
-//					s.setSession_id(jobj.getString("session_id"));
-//					//s.setSlots(jobj.getString(""));
-//					s.setState_name(jobj.getString("state_name"));
-//					s.setTo(jobj.getString("to"));
-//					s.setVaccine(jobj.getString("vaccine"));
-//
-//
-//					listdata.add(s);
-//				}
-//
-//				List<Sessions> availableSessions = listdata.stream().filter(sessions -> sessions.getFee().equals("0"))
-//						.filter(sessions -> sessions.getMin_age_limit().equals("18"))
-//						.filter(sessions -> sessions.getAvailable_capacity() > 0)
-//						.collect(Collectors.toList());
-//
-//				if(availableSessions!=null || availableSessions.size()>0){
-//					availableSessions.stream().forEach(sessions->{
-//						System.out.println("Vaccine available at " + sessions.getPincode());
-//					});
-//				} else {
-//					System.out.println("No vaccine available");
-//				}
-//			}
-//
-//		};
-//	}
 }
